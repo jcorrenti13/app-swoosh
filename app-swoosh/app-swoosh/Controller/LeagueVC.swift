145b1f34b445678e3397a4fba8f2867d2e09edc7
Jake Correnti <jakecorrenti@Jakes-MacBook-Pro.local>
2019-05-11 10:49:48 -0400

    added logic for the league screen and its buttons

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,13 +10,34 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    @IBOutlet weak var nextButton: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        player = Player()
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
-
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeage(leagueType: "Mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeage(leagueType: "Womens")
+    }
+    
+    @IBAction func onCoEdTapped(_ sender: Any) {
+        selectLeage(leagueType: "Co-Ed")
+    }
+    
+    // never have logic built into IBActions, make a separate function for that
+    func selectLeage(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
+    }
 }
